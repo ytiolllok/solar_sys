023d144b58f48771b8049c4b393d56171ef6e757
Yurii <prouranium@gmail.com>
2020-11-12 19:41:13 +0300

    draw fix

--- solar_vis.py
+++ solar_vis.py
@@ -57,7 +57,7 @@ def scale_y(y):
 
     **y** — y-координата модели.
     """
-    pass  # FIXME
+    return -int(y*scale_factor) + window_height//2
 
 
 
@@ -85,4 +85,4 @@ class DrawableObject:
         self.obj = obj
 
     def draw(self, surface):
-            pass  # FIXME
+        pg.circle(surface, self.color, (self.x, self.y), self.R)
